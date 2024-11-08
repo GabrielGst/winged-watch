f8b413950e91414e3526c5b2b8eb3b23e95b7c92
GabrielGst <gabriel@webagab.xyz>
2024-11-08 12:55:34 +0100

    Understood db filenaming method and visualized wind db.

--- bdd/spire.py
+++ bdd/spire.py
@@ -6,14 +6,41 @@
 #     print("{}, {}, {}".format(v, ds[v].attrs["long_name"], ds[v].attrs["units"]))
     
 import cfgrib
+import pandas
 
 # Load all layers, separated into individual datasets
 datasets = cfgrib.open_datasets("./bdd/arome__0025__SP1__00H06H__2024-11-07T21_00_00Z.grib2")
 
 # Print information about each dataset
-for i, ds in enumerate(datasets):
-    print(f"Dataset {i}:")
-    print("\n")
-    print(ds)
-    print("\n")
-    print("Next:\n")
+# for i, ds in enumerate(datasets):
+#     print(f"Dataset {i}:")
+#     print("\n")
+#     print(ds)
+#     print("\n")
+#     print("Next:\n")
+
+
+wind_speed = datasets[1].si10
+wind_direction = datasets[1].wdir10
+
+# print(wind_speed)
+# print(wind_direction)
+
+df = wind_direction.to_dataframe()
+df2 = wind_speed.to_dataframe()
+
+df.head(10).style
+
+
+# import plotly.express as px
+
+# fig = px.scatter_geo(
+#     df, locations="iso_alpha",
+#     size="pop", # size of markers, "pop" is one of the columns of gapminder
+# )
+
+# fig.show()
+
+# df.to_csv("wind_direction.csv", index=False)
+
+# df2.to_csv("wind_speed.csv", index=False)
