51fa2f4625f234a399fa3fe774b290c5128b64f7
GabrielGst <gabriel@webagab.xyz>
2024-11-08 11:24:24 +0100

    Exported conda env to yml file.

--- bdd/spire.py
+++ bdd/spire.py
@@ -8,9 +8,12 @@
 import cfgrib
 
 # Load all layers, separated into individual datasets
-datasets = cfgrib.open_datasets("./bdd/arome__001__HP1__00H__2024-11-07T09_00_00Z.grib2")
+datasets = cfgrib.open_datasets("./bdd/arome__0025__SP1__00H06H__2024-11-07T21_00_00Z.grib2")
 
 # Print information about each dataset
 for i, ds in enumerate(datasets):
     print(f"Dataset {i}:")
+    print("\n")
     print(ds)
+    print("\n")
+    print("Next:\n")
